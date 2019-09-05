914ee96fecd9f5b233b0e8eeb839ccaa2eee519a
Wanderson Silva <bsilva.wanderson@gmail.com>
2019-09-04 21:16:52 -0300

    adding hamming window

--- spectral-audio-analyzer.m
+++ spectral-audio-analyzer.m
@@ -3,57 +3,63 @@ clear;
 
 %Frequência de amostragem igual a 8kHz, números de bits da codificação e
 %numeros de canais(estéreo)
-Fs = 8000;
+%Fs = 8000;
 nbits = 8;
 ch = 1;
 
 %AQUISIÇÃO DO ÁUDIO=================================================
 %Cria um objeto para a gravação do áudio com a frequência de amostragem de 8kHz e 8 bits por amostra 
-objetoGravacao = audiorecorder(Fs, nbits, ch);
+%%%%%objetoGravacao = audiorecorder(Fs, nbits, ch);
 
 %Grava o áudio até o usuário pressionar enter
 disp('========GRAVAÇÃO INICIADA========');
 disp('Pressione enter para finalizar...');
-record(objetoGravacao);
-input('');
+%%%%%record(objetoGravacao);
+%%%%%input('');
 
 %Armazena a amplitude do sinal em cada instante de gravação
-audioOriginalTempo = getaudiodata(objetoGravacao);
-
+%%%%audioOriginalTempo = getaudiodata(objetoGravacao);
+[audioOriginalTempo,Fs] = audioread('sinal1.wav')
 
 %JANELAMENTO DO AUDIO===============================================
-%audioJanelado = audioOriginalTempo * hamming(audioOriginalTempo)
+audioJaneladoTempo = audioOriginalTempo .* hamming(length(audioOriginalTempo),'periodic')
 
 
 
 %TRANSFORMADA DO ÁUDIO============================================
 %Calcula a FFT
 audioOriginalFrequencia = fft(audioOriginalTempo);
-%audioOriginalFrequencia = fft(audioJanelado);
+audioJaneladoFrequencia = fft(audioJaneladoTempo);
 
 %Desloca ela na frequência para mostrar em 0Hz
-audioOriginalFrequencia = fftshift(audioOriginalFrequencia); 
+%audioOriginalFrequencia = fftshift(audioOriginalFrequencia); 
+%audioJaneladoFrequencia = fftshift(audioJaneladoFrequencia); 
+
 
 %Pega o número de amostras da transformada
 n = length(audioOriginalTempo);
 
 %A frequência de range
-f = (-n/2:n/2-1)*(Fs/n);      
+%f = (-n/2:n/2-1)*(Fs/n);      
+f = (0:length(audioOriginalTempo)-1) * (Fs/n)
 
 %Calcula o módulo tirado a parte conjugada(fase) da transformada
 audioOriginalFrequenciaE = abs(audioOriginalFrequencia).^2/n;    
+audioJaneladoFrequenciaE = abs(audioJaneladoFrequencia).^2/n;    
 
 
 %OPÇÕES DE VISUALIZAÇÃO DE RESULTADOS===============================
 escolha = 1;
-while escolha ~= 2
+while escolha ~= 5
     clc;
-    escolha = input('[1] - Execução do áudio original\n[2] - Plotar gráficos e sair\nEscolha: ');
+    escolha = input('[1] - Execução do áudio \n[2] - Plotar sinal original \n[3] - Plotar sinal janelado \n[4] - Comparar sinais no tempo \n[5] sair \nEscolha: ');
+    disp('kkkkkkkkkkkkkkkkkkkkkkkkk')
+    disp(Fs)
     if escolha == 1
         sound(audioOriginalTempo);
     elseif escolha == 2
         subplot(211), plot(audioOriginalTempo, 'b'),
-        axis tight, title('Áudio no domínio do Tempo'),
+        axis tight, title('Áudio original (Tempo)'),
         xlabel('tempo(s)')
         ylabel('x(t)')
 
@@ -62,6 +68,27 @@ while escolha ~= 2
         axis tight, title('Áudio no domínio da Frequência'),
         xlabel('Frequência(Hz)')
         ylabel('Amplitude')
+    elseif escolha == 3
+        subplot(211), plot(audioJaneladoTempo, 'b'),
+        axis tight, title('Áudio Janelado (Tempo)'),
+        xlabel('tempo(s)')
+        ylabel('x(t)')
+
+        subplot(212), plot(f, audioJaneladoFrequenciaE, 'b'),
+        legend('Normal'),
+        axis tight, title('Áudio no domínio da Frequência'),
+        xlabel('Frequência(Hz)')
+        ylabel('Amplitude')
+    elseif escolha == 4
+        subplot(211), plot(audioOriginalTempo, 'b'),
+        axis tight, title('Áudio original (Tempo)'),
+        xlabel('tempo(s)')
+        ylabel('x(t)')
+        
+        subplot(212), plot(audioJaneladoTempo, 'b'),
+        axis tight, title('Áudio janelado (Tempo)'),
+        xlabel('tempo(s)')
+        ylabel('g(t)')      
     end
     
 end
