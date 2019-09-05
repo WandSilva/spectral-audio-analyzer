3c0ee71bb150d7750d240d313852f3689383c309
Wanderson Silva <bsilva.wanderson@gmail.com>
2019-09-04 21:30:43 -0300

    option to choose between writing a new signal or reading an existing signal on disk

--- spectral-audio-analyzer.m
+++ spectral-audio-analyzer.m
@@ -3,38 +3,41 @@ clear;
 
 %Frequência de amostragem igual a 8kHz, números de bits da codificação e
 %numeros de canais(estéreo)
-%Fs = 8000;
+Fs = 8000;
 nbits = 8;
 ch = 1;
 
-%AQUISIÇÃO DO ÁUDIO=================================================
-%Cria um objeto para a gravação do áudio com a frequência de amostragem de 8kHz e 8 bits por amostra 
-%%%%%objetoGravacao = audiorecorder(Fs, nbits, ch);
+escolha = 0;
+while escolha ~= 10
+    clc;
+    escolha = input('[1] - Gravar novo sinal \n[2] - Ler arquivo \nEscolha: ');
+    if escolha == 1
+        %AQUISIÇÃO DO ÁUDIO=================================================
+        %Cria um objeto para a gravação do áudio com a frequência de amostragem de 8kHz e 8 bits por amostra 
+        objetoGravacao = audiorecorder(Fs, nbits, ch);
 
-%Grava o áudio até o usuário pressionar enter
-disp('========GRAVAÇÃO INICIADA========');
-disp('Pressione enter para finalizar...');
-%%%%%record(objetoGravacao);
-%%%%%input('');
+        %Grava o áudio até o usuário pressionar enter
+        disp('========GRAVAÇÃO INICIADA========');
+        disp('Pressione enter para finalizar...');
+        record(objetoGravacao);
+        input('');
+        %Armazena a amplitude do sinal em cada instante de gravação
+        audioOriginalTempo = getaudiodata(objetoGravacao);
+        escolha = 10
+    elseif escolha == 2
+        [audioOriginalTempo,Fs] = audioread('sinal1.wav')
+        escolha = 10
 
-%Armazena a amplitude do sinal em cada instante de gravação
-%%%%audioOriginalTempo = getaudiodata(objetoGravacao);
-[audioOriginalTempo,Fs] = audioread('sinal1.wav')
+    end
+end
 
 %JANELAMENTO DO AUDIO===============================================
 audioJaneladoTempo = audioOriginalTempo .* hamming(length(audioOriginalTempo),'periodic')
-
-
 
 %TRANSFORMADA DO ÁUDIO============================================
 %Calcula a FFT
 audioOriginalFrequencia = fft(audioOriginalTempo);
 audioJaneladoFrequencia = fft(audioJaneladoTempo);
-
-%Desloca ela na frequência para mostrar em 0Hz
-%audioOriginalFrequencia = fftshift(audioOriginalFrequencia); 
-%audioJaneladoFrequencia = fftshift(audioJaneladoFrequencia); 
-
 
 %Pega o número de amostras da transformada
 n = length(audioOriginalTempo);
@@ -53,8 +56,6 @@ escolha = 1;
 while escolha ~= 5
     clc;
     escolha = input('[1] - Execução do áudio \n[2] - Plotar sinal original \n[3] - Plotar sinal janelado \n[4] - Comparar sinais no tempo \n[5] sair \nEscolha: ');
-    disp('kkkkkkkkkkkkkkkkkkkkkkkkk')
-    disp(Fs)
     if escolha == 1
         sound(audioOriginalTempo);
     elseif escolha == 2
